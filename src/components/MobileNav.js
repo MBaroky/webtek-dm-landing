a87a9d9dd5abef87bc099271346fbbff22978afc
Mohamed Gamal El Baroky <m.baroky@gmail.com>
2024-05-08 22:02:40 +0300

    tuning mobile menu

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -3,10 +3,10 @@ import {
   Sheet,
   SheetContent,
   SheetTrigger,
-  // SheetClose,
+  SheetClose,
 } from "./ui/sheet";
 import { Button } from "./ui/button";
-import { Menu as MenuIcon } from "lucide-react";
+import { Menu as MenuIcon, X as CloseIcon } from "lucide-react";
 import { navItems } from "../data/navItems";
 import NavItem from "./NavItem";
 import BookSheet from "./BookSheet";
@@ -25,11 +25,20 @@ export default function MobileNav() {
 
       <SheetContent side='top' className='bg-black text-white'>
         <div className='flex flex-col items-center pb-20 mx-auto w-full text-base font-bold text-white max-w-[480px]'>
-          <a
-            href='/'
-            className='justify-center items-center self-stretch px-10 pt-10 pb-px w-full text-xl font-semibold uppercase bg-black text-center'>
-            Digital Marketing
-          </a>
+          <header className='bg-[#000] flex justify-center items-center px-16 py-4 font-medium text-white bg-neutral-900 max-md:px-5 fixed top-0 left-0 w-full z-50'>
+            <div className='flex gap-5 items-center w-full max-w-[1196px] max-md:flex-wrap max-md:max-w-full'>
+              <a
+                href='/'
+                className='flex-auto self-stretch my-auto text-xl font-semibold uppercase'>
+                Digital Marketing
+              </a>
+            </div>
+            <SheetClose>
+              <Button variant='ghost' size='icon'>
+                <CloseIcon />
+              </Button>
+            </SheetClose>
+          </header>
           <nav className='my-24 flex flex-col justify-center items-center gap-4'>
             {navItems.map((item, index) => (
               <NavItem key={item.label} {...item} />
